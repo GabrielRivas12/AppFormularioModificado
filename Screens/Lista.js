cbc2e9d1b1240a8b06782731e2313eabcc044e63
gabriel <gabrielzero2bi@gmail.com>
2025-05-13 18:51:22 -0600

    subida de archivos

---
+++ Screens/Lista.js
@@ -0,0 +1,170 @@
+import { View, Button, StyleSheet, Text, TouchableOpacity, Alert } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import { useNavigation } from '@react-navigation/native';
+import React, { useState } from 'react';
+
+
+export default function Lista() {
+
+    const navigation = useNavigation();
+
+    const eliminar = (index) => {
+        Alert.alert(
+            'Confirmar aliminacion',
+            'Estas seguro de que deseas elimanar este cliente?',
+            [
+                {
+                    text: 'Cancelar',
+                    style: 'cancel',
+                },
+                {
+                    text: 'Eliminar',
+                    style: 'destructive',
+                    onPress: () => {
+                        const nuevaLista = [ ...clientes];
+                        nuevaLista.splice(index,1);
+                        setClientes(nuevaLista);
+                    }
+                },
+            ],
+            { cancelable: true}
+        );
+    }
+    
+    const [clientes, setClientes] = useState([
+
+
+        {
+            Ncedula: '1111',
+            Nnombres: 'jjj',
+            Napellidos: 'nnn',
+            Nfechanac: '1111',
+            Nsexo: 'Masculino'
+
+        },
+
+           {
+            Ncedula: '1111',
+            Nnombres: 'jjj',
+            Napellidos: 'nnn',
+            Nfechanac: '1111',
+            Nsexo: 'Masculino'
+
+        }
+
+
+    ]);
+
+  const guardaNuevo = (nuevo) => {
+    setClientes([nuevo, ...clientes])
+  }
+
+
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity style={styles.boton} onPress={() => navigation.navigate('Formulario', {guardaNuevo})} >
+
+                <FontAwesome5 name="user-plus" size={24} color="#4F8B2E" />
+            </TouchableOpacity>
+            <Text style={styles.titulo}> Lista de clientes </Text>
+
+            {clientes.length == 0 ? (
+                <Text style={styles.mensaje}> No hay clientes</Text>
+
+            ) : (
+
+                <ScrollView style={styles.scroll}>
+                    {clientes.map((clientes, index) => (
+                        <View key={index} style={styles.card}>
+
+                            <Text style={styles.label}> Cedula: <Text style={styles.valor}> {clientes.Ncedula} </Text> </Text>
+
+
+                                <TouchableOpacity style={styles.botone} onPress={(eliminar)} >
+
+                                    <FontAwesome5 name="trash" size={24} color="red" />
+                                </TouchableOpacity>
+
+
+
+                            <Text style={styles.label}> Nombre: <Text style={styles.valor}> {clientes.Nnombres} </Text> </Text>
+                            <Text style={styles.label}> Apeliidos: <Text style={styles.valor}> {clientes.Napellidos} </Text> </Text>
+                            <Text style={styles.label}> Fecha de nacimiento: <Text style={styles.valor}> {clientes.Nfechanac} </Text> </Text>
+                            <Text style={styles.label}> Sexo: <Text style={styles.valor}> {clientes.Nsexo} </Text> </Text>
+                        </View>
+
+                    ))}
+
+                </ScrollView>
+
+            )}
+
+
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#E6F7E6',
+    },
+    titulo: {
+        fontSize: 25,
+        paddingBottom: 10,
+        fontWeight: 'bold',
+        color: '#4F8B2E',
+        paddingTop: 20
+    },
+    card: {
+        height: 130,
+        width: 360,
+        backgroundColor: '#C2E8C2',
+        alignItems: 'left',
+        justifyContent: 'center',
+        borderRadius: 5,
+        marginBottom: 10,
+        position: 'relative'
+    },
+    label: {
+        color: '#00000',
+        marginLeft: 10,
+        paddingTop: 1,
+
+    },
+    valor: {
+        color: '#358B47'
+
+    },
+    boton: {
+        height: 50,
+        backgroundColor: '#E6F7E6',
+        width: 50,
+        borderRadius: 10,
+        borderColor: '#4F8B2E',
+        borderWidth: 1,
+        marginLeft: 300,
+        justifyContent: 'center',
+        alignItems: 'center',
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 1,
+        },
+        shadowOpacity: 0.22,
+        shadowRadius: 2.22,
+
+        elevation: 3,
+
+    },
+    botone: {
+        position: 'absolute',
+        top: 10,
+        right: 10,
+        width: 32,
+        height: 32,
+    }
+});
